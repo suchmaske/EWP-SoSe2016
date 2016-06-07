85efbb8b609cb51a1a8f12c71f2d5f57e8b08c05
Oliver Pohl <opohl@bbaw.de>
2016-06-07 13:53:21 +0200

    Added session 06 Material

---
+++ sessions/06/JS-js-vs-jquery-js.js
@@ -0,0 +1,3 @@
+let btn = document.getElementsByName("button");
+let btn2 = document.getElementsByClassName("btn");
+let btn3 = document.getElementById("add");
---
+++ sessions/06/Js-js-vs-jquery-jquery.js
@@ -0,0 +1,3 @@
+let btn = $("button");
+let btn2 = $(".btn");
+let btn3 = $("#add");
